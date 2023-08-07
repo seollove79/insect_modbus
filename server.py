2ca4e5491eb29de0bfa9a1e0730e940af605631d
seollove79 <seollove@gmail.com>
2023-08-07 22:09:42 +0900

    .

--- server.py
+++ server.py
@@ -60,14 +60,16 @@ if __name__ == '__main__':
     
     def writethread():
         while True :
-            if myDataBank.write_check == 1:
-                #장치의 제어값 만들기
-                controlValue01 = '@,1,' + changeNumber(myDataBank.sensor_data[17]) + ',' + changeNumber(myDataBank.sensor_data[18])  + ',' + changeNumber(myDataBank.sensor_data[19]) + ',' + changeNumber(myDataBank.sensor_data[20]) + '\r\n'
-                controlValue02 = '@,2,' + changeNumber(myDataBank.sensor_data[37]) + ',' + changeNumber(myDataBank.sensor_data[38])  + ',' + changeNumber(myDataBank.sensor_data[39]) + ',' + changeNumber(myDataBank.sensor_data[40]) + '\r\n'
-                controlValue03 = '@,3,' + changeNumber(myDataBank.sensor_data[57]) + ',' + changeNumber(myDataBank.sensor_data[58])  + ',' + changeNumber(myDataBank.sensor_data[59]) + ',' + changeNumber(myDataBank.sensor_data[60]) + '\r\n'
+            
+            #장치의 제어값 만들기
+            controlValue01 = '@,1,' + changeNumber(myDataBank.sensor_data[17]) + ',' + changeNumber(myDataBank.sensor_data[18])  + ',' + changeNumber(myDataBank.sensor_data[19]) + ',' + changeNumber(myDataBank.sensor_data[20]) + '\r\n'
+            controlValue02 = '@,2,' + changeNumber(myDataBank.sensor_data[37]) + ',' + changeNumber(myDataBank.sensor_data[38])  + ',' + changeNumber(myDataBank.sensor_data[39]) + ',' + changeNumber(myDataBank.sensor_data[40]) + '\r\n'
+            controlValue03 = '@,3,' + changeNumber(myDataBank.sensor_data[57]) + ',' + changeNumber(myDataBank.sensor_data[58])  + ',' + changeNumber(myDataBank.sensor_data[59]) + ',' + changeNumber(myDataBank.sensor_data[60]) + '\r\n'
 
-                #장치의 제어값 보드에 쓰기
-                controller.writeControlValue("!,0,1\r\n")
+            #장치의 제어값 보드에 쓰기
+            controller.writeControlValue("!,0,1\r\n")
+            time.sleep(3)
+            if myDataBank.write_check == 1:
                 controller.writeControlValue(controlValue01)
                 time.sleep(1)
                 controller.writeControlValue(controlValue02)
@@ -77,9 +79,9 @@ if __name__ == '__main__':
                 myDataBank.write_check = 0
                 print("보냈음")
 
-                myDataBank.sensor_data.update(controller.data1)
-                myDataBank.sensor_data.update(controller.data2)
-                myDataBank.sensor_data.update(controller.data3)
+            myDataBank.sensor_data.update(controller.data1)
+            myDataBank.sensor_data.update(controller.data2)
+            myDataBank.sensor_data.update(controller.data3)
 
             time.sleep(3)
 
